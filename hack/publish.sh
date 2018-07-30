fe8982703474d0c8a73b18e41376b435dc7f9076
Scott Andrews <sandrews@pivotal.io>
2018-07-30 11:58:02 -0400

    repo rename riff-init -> riff- build

--- hack/publish.sh
+++ hack/publish.sh
@@ -6,12 +6,12 @@ set -o pipefail
 
 version=`cat VERSION`
 
-echo "Publishing riff-init"
+echo "Publishing riff-build"
 
 # build and publish containers, generate yaml
-ko resolve -f config/ > riff-init.yaml
+ko resolve -f config/ > riff-build.yaml
 
 # publish yaml
-gsutil cp -a public-read "riff-init.yaml" "gs://riff-releases/latest/riff-init.yaml"
-gsutil cp -a public-read "riff-init.yaml" "gs://riff-releases/previous/riff-init/riff-init-${version}.yaml"
-gsutil cp -a public-read "riff-init.yaml" "gs://riff-releases/previous/riff-init/riff-init-${version}-ci-${TRAVIS_COMMIT}.yaml"
+gsutil cp -a public-read "riff-build.yaml" "gs://riff-releases/latest/riff-build.yaml"
+gsutil cp -a public-read "riff-build.yaml" "gs://riff-releases/previous/riff-build/riff-build-${version}.yaml"
+gsutil cp -a public-read "riff-build.yaml" "gs://riff-releases/previous/riff-build/riff-build-${version}-ci-${TRAVIS_COMMIT}.yaml"
